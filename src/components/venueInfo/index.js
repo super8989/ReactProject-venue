158a78c9008fd1a1bc985e6aed2f92930c73dab0
Sam <super8989@gmail.com>
2019-05-08 21:16:19 -0400

    VenueInfo component

---
+++ src/components/venueInfo/index.js
@@ -0,0 +1,53 @@
+import React from "react";
+import Zoom from "react-reveal/Zoom";
+
+import icon_calendar from "../../resources/images/icons/calendar.png";
+import icon_location from "../../resources/images/icons/location.png";
+
+const VenueInfo = () => {
+  return (
+    <div className="bck_black">
+      <div className="center_wrapper">
+        <div className="vn_wrapper">
+
+          <Zoom duration={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_red" />
+                  <div
+                    className="vn_icon"
+                    style={{ background: `url(${icon_calendar})` }}
+                  />
+                  <div className="vn_title">Event Date & Time</div>
+                  <div className="vn_desc">December 16, 2019 @ 7:00 PM</div>
+                </div>
+              </div>
+            </div>
+          </Zoom>
+
+          <Zoom duration={500} delay={500}>
+            <div className="vn_item">
+              <div className="vn_outer">
+                <div className="vn_inner">
+                  <div className="vn_icon_square bck_yellow" />
+                  <div
+                    className="vn_icon"
+                    style={{ background: `url(${icon_location})` }}
+                  />
+                  <div className="vn_title">Event Location</div>
+                  <div className="vn_desc">Rogers Centre, Toronto, ON</div>
+
+                  <div />
+                </div>
+              </div>
+            </div>
+          </Zoom>
+          
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default VenueInfo;
