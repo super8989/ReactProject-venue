52224eabbbfec0e499239c0c663ff0d35547f13e
Sam <super8989@gmail.com>
2019-05-11 18:26:35 -0400

    adding a scrollTo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React, { Component } from 'react';
-import './resources/styles.css';
+import React, { Component } from "react";
+import "./resources/styles.css";
+import { Element } from "react-scroll";
 
-import Header from './components/header_footer/header';
-import Featured from './components/featured';
-import VenueInfo from './components/venueInfo';
-import Highlight from './components/Highlights';
-import Pricing from './components/pricing';
-import Location from './components/location';
-import Footer from './components/header_footer/Footer';
+import Header from "./components/header_footer/header";
+import Featured from "./components/featured";
+import VenueInfo from "./components/venueInfo";
+import Highlight from "./components/Highlights";
+import Pricing from "./components/pricing";
+import Location from "./components/location";
+import Footer from "./components/header_footer/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ height: '1500px', background: 'cornflowerblue' }}>
+      <div
+        className="App"
+        style={{ height: "1500px", background: "cornflowerblue" }}
+      >
         <Header />
-        <Featured />
-        <VenueInfo />
-        <Highlight />
-        <Pricing />
-        <Location />
+
+        <Element name="featured">
+          <Featured />
+        </Element>
+        <Element name="venueinfo">
+          <VenueInfo />
+        </Element>
+        <Element name="highlights">
+          <Highlight />
+        </Element>
+        <Element name="pricing">
+          <Pricing />
+        </Element>
+        <Element name="location">
+          <Location />
+        </Element>
+
         <Footer />
       </div>
     );
